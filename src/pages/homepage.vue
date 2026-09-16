e2cfb5d350d26000f54ba01ea3dedbb027caf7cb
Kenan Ustalic <kenan_ustalic@sluz.ch>
2026-09-16 09:45:29 +0200

    buttons angepasst

--- src/pages/homepage.vue
+++ src/pages/homepage.vue
@@ -23,32 +23,21 @@
         </button>
 
         <input
+          v-model="searchQuery"
           type="text"
           placeholder="Klassen suchen"
         >
 
         <ul>
-          <li>
-            <button type="button">
-              KLA
-            </button>
-          </li>
-
-          <li>
-            <button type="button">
-              KLB
-            </button>
-          </li>
-
-          <li>
-            <button type="button">
-              KLC
-            </button>
-          </li>
-
-          <li>
-            <button type="button">
-              KLD
+          <li
+            v-for="klasse in filteredClasses"
+            :key="klasse.id"
+          >
+            <button
+              type="button"
+              @click="selectClass(klasse.id)"
+            >
+              {{ klasse.name }}
             </button>
           </li>
         </ul>
@@ -60,7 +49,68 @@
 
       <aside>
         <h2>Klasse bearbeiten</h2>
+
+        <div v-if="selectedClass">
+          <h3>{{ selectedClass.name }}</h3>
+
+          <p>
+            Lehrgang: {{ selectedClass.course }}
+          </p>
+        </div>
+
+        <p v-else>
+          Keine Klasse ausgewählt.
+        </p>
       </aside>
     </main>
   </div>
 </template>
+
+<script setup>
+import { computed, ref } from 'vue'
+
+const classes = ref([
+  {
+    id: 1,
+    name: 'KLA',
+    course: 'IMS'
+  },
+  {
+    id: 2,
+    name: 'KLB',
+    course: 'WayUp'
+  },
+  {
+    id: 3,
+    name: 'KLC',
+    course: 'IMS'
+  },
+  {
+    id: 4,
+    name: 'KLD',
+    course: 'WayUp'
+  }
+])
+
+const searchQuery = ref('')
+
+const selectedClassId = ref(null)
+
+const filteredClasses = computed(() => {
+  return classes.value.filter((klasse) => {
+    return klasse.name
+      .toLowerCase()
+      .includes(searchQuery.value.toLowerCase())
+  })
+})
+
+const selectedClass = computed(() => {
+  return classes.value.find((klasse) => {
+    return klasse.id === selectedClassId.value
+  })
+})
+
+function selectClass(classId) {
+  selectedClassId.value = classId
+}
+</script>
